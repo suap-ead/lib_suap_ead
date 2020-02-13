d0e57aa9db04a6c40c18db509a818e4e498f7212
Kelson da Costa Medeiros <kelson.costa@escolar.ifrn.edu.br>
2020-02-13 20:07:27 -0300

    [FIX] quebrava na tela de login pois ainda não tinha sessão criada

--- suap_ead/context_processors.py
+++ suap_ead/context_processors.py
@@ -1,2 +1,4 @@
 def suap_ead(request):
-    return {'suap_ead': request.session['suap_ead']}
+    if 'suap_ead' in request.session:
+        return {'suap_ead': request.session['suap_ead']}
+    return {'suap_ead': None}
