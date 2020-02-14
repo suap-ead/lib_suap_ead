13daa23afb7dc52b39d6be89889e2f1e7cfa8a7c
Kelson da Costa Medeiros <kelson.costa@escolar.ifrn.edu.br>
2020-02-14 12:55:16 -0300

    Release suap_ead 0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     packages=['suap_ead'],
     include_package_data=True,
     version='0.1.0',
-
     download_url='https://github.com/suap-ead/lib_suap_ead/releases/tag/0.1.0',
     url='https://github.com/suap-ead/lib_suap_ead',
     keywords=['SUAP', 'EAD', 'complemento', 'JWT', 'Django', 'Auth', 'SSO', 'client', 'Theme', ],
