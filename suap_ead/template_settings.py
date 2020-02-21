a9dc22c920b1454cf82dd0ca9eb1665a5f3a0b85
Kelson da Costa Medeiros <kelson.costa@escolar.ifrn.edu.br>
2020-02-21 12:03:05 -0300

    [ADD] OpenAPI Swagger & ReDoc

--- suap_ead/template_settings.py
+++ suap_ead/template_settings.py
@@ -98,6 +98,7 @@ USE_TZ = env_as_bool('DJANGO_USE_TZ', True)
 
 # REST Framework
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
